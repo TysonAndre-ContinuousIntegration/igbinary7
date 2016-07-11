d4b8ecaba791602e9cf13d9b981603cb69f76e2d
TysonAndre-ContinuousIntegration <tysonandre775+github@gmail.com>
2016-07-11 14:19:46 -0400

    handle non-reference array, don't print an error

--- igbinary.c
+++ igbinary.c
@@ -1138,6 +1138,9 @@ inline static int igbinary_serialize_array_ref(struct igbinary_serialize_data *i
           printf("\nUsing regular ref is_ref=%d type=%d key=%lld\n", (int)Z_ISREF_P(z), (int)Z_TYPE_P(z), (long long) key);
 #endif
 	/* } */
+	} else if (Z_TYPE_P(z) == IS_ARRAY) {
+		// igbinary_serialize_array calls this, whether or not this is a reference or not.
+		key = (zend_ulong) (zend_uintptr_t) Z_ARR_P(z);
 	} else {
 		// Nothing else is going to reference this when this is serialized, this isn't ref counted or an object. Increment the reference id for the deserializer, give up.
 		++igsd->references_id;
